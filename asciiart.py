861d05ea56361a9201185fc9f69d72b5eea6d86b
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2021-04-11 13:34:10 +0900

    first commit

---
+++ asciiart.py
@@ -0,0 +1,23 @@
+# asciiart.py
+
+for y in range(-12,12):
+    for x in range(-39,39):
+        ca=x*0.0458
+        cb=y*0.08333
+        a=ca
+        b=cb
+        for i in range(0,15):
+            t=a*a-b*b+ca
+            b=2*a*b+cb
+            a=t
+            flag=0
+            if (a*a+b*b)>4:
+                flag = 1
+                break
+        if flag == 0:
+            print(" ", end="")
+        else:
+            if i>9:
+                i=i+7
+            print(chr(48+i), end="")
+    print(" ")
